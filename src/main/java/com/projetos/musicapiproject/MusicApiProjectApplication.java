8d4ab00f02769510dc5d73d012083d0aee331ab5
MazuttiGuilherme <mazuttimail@gmail.com>
2024-07-02 11:33:23 -0300

    Criando Entidade Music

--- src/main/java/com/projetos/musicapiproject/MusicApiProjectApplication.java
+++ src/main/java/com/projetos/musicapiproject/MusicApiProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MusicApiProjectApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MusicApiProjectApplication.class, args);
     }
 
--- src/main/java/com/projetos/musicapiproject/musicapi/model/Music.java
+++ src/main/java/com/projetos/musicapiproject/musicapi/model/Music.java
@@ -1,4 +1,23 @@
 package com.projetos.musicapiproject.musicapi.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Music {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String artist;
+    private String album;
+    private int year;
+
 }
