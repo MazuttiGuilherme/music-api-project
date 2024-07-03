f7ffaf30d9bb8144b75b6b29cc61e493c162b207
MazuttiGuilherme <mazuttimail@gmail.com>
2024-07-03 12:11:34 -0300

    Corrigindo SecurityConfig e finalizando projeto

--- src/main/java/com/projetos/musicapiproject/musicapi/config/SecurityConfig.java
+++ src/main/java/com/projetos/musicapiproject/musicapi/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -28,6 +29,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/music/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/music/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/music/**").hasRole("ADMIN")
+                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(withDefaults());
